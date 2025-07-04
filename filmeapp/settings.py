19f9105c7496c723451c00f9e66af54dfc0d3c4d
joaoedley <edleyjoao@gmail.com>
2025-07-03 23:29:50 -0300

    pequna att no settings

--- filmeapp/settings.py
+++ filmeapp/settings.py
@@ -4,7 +4,6 @@ Django settings for filmeapp project.
 
 from pathlib import Path
 import os
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,7 +63,10 @@ WSGI_APPLICATION = 'filmeapp.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR}/db.sqlite3', conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
